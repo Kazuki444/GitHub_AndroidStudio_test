0eed9c41146f433daef767ccaf21eedc927877b8
Kazuki444 <otsuka.kazuki@g.wakayama-u.jp>
2021-12-10 11:39:21 +0900

    サブPCで編集

--- app/src/main/java/com/kazuki/github_androidstudio_test/MainActivity.java
+++ app/src/main/java/com/kazuki/github_androidstudio_test/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
   private static final String TAG=MainActivity.class.getSimpleName();
 
   private final String message1 = "from main PC";
+
+  private final String message2 = "edit from sub PC";
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
